81fd471dfa9011d80645492721b444591c14fa89
shanlu457 <shanlu457@gmail.com>
2023-06-14 23:48:54 +0000

    comment

---
+++ src/components/Comments/index.js
@@ -0,0 +1,136 @@
+import {Component} from 'react'
+import {v4} from 'uuid'
+import CommentItem from '../CommentItem'
+import './index.css'
+
+const initialContainerBackgroundClassNames = [
+  'amber',
+  'blue',
+  'orange',
+  'emerald',
+  'teal',
+  'red',
+  'light-blue',
+]
+
+class Comments extends Component {
+  state = {
+    commentsList: [],
+    nameInput: '',
+    commentInput: '',
+  }
+
+  onChangeNameInput = event => {
+    this.setState({nameInput: event.target.value})
+  }
+
+  onChangeCommentInput = event => {
+    this.setState({commentInput: event.target.value})
+  }
+
+  onAddButton = event => {
+    event.preventDefault()
+    const {nameInput, commentInput} = this.state
+    const initialBackgroundClassName = `initial-container ${
+      initialContainerBackgroundClassNames[
+        Math.ceil(
+          Math.random() * initialContainerBackgroundClassNames.length - 1,
+        )
+      ]
+    }`
+    const newComment = {
+      id: v4(),
+      name: nameInput,
+      comment: commentInput,
+      date: new Date(),
+      isLiked: false,
+      initialClassName: initialBackgroundClassName,
+    }
+    this.setState(prevState => ({
+      commentsList: [...prevState.commentsList, newComment],
+      nameInput: '',
+      commentInput: '',
+    }))
+  }
+
+  toggleIsLiked = id => {
+    this.setState(prevState => ({
+      commentsList: prevState.commentsList.map(eachComment => {
+        if (eachComment.id === id) {
+          return {...eachComment, isLiked: !eachComment.isLiked}
+        }
+        return eachComment
+      }),
+    }))
+  }
+
+  deleteComment = commentId => {
+    const {commentsList} = this.state
+    this.setState({
+      commentsList: commentsList.filter(
+        eachComment => eachComment.id !== commentId,
+      ),
+    })
+  }
+
+  renderCommentsList = () => {
+    const {commentsList} = this.state
+    return commentsList.map(eachComment => (
+      <CommentItem
+        key={eachComment.id}
+        commentDetails={eachComment}
+        toggleIsLiked={this.toggleIsLiked}
+        deleteComment={this.deleteComment}
+      />
+    ))
+  }
+
+  render() {
+    const {nameInput, commentInput, commentsList} = this.state
+
+    return (
+      <div className="app-container">
+        <div className="comments-container">
+          <h1 className="app-heading">Comments</h1>
+          <div className="comments-inputs">
+            <form className="form" onSubmit={this.onAddButton}>
+              <p className="form-description">
+                Say something about 4.0 Technologies
+              </p>
+              <input
+                type="text"
+                className="name-input"
+                placeholder="Your Name"
+                value={nameInput}
+                onChange={this.onChangeNameInput}
+              />
+              <textarea
+                className="comment-input"
+                placeholder="Your Comment"
+                rows="6"
+                value={commentInput}
+                onChange={this.onChangeCommentInput}
+              />
+              <button className="add-button" type="submit">
+                Add Comment
+              </button>
+            </form>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+              className="image"
+              alt="comments"
+            />
+          </div>
+          <hr className="line" />
+          <p className="heading">
+            <span className="comments-count">{commentsList.length}</span>
+            Comments
+          </p>
+          <ul className="comments-list">{this.renderCommentsList()}</ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Comments
